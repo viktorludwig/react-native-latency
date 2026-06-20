dc54cb1e1a9c563e6ab14d3b987bae551ab8066e
Viktor Ludwig <viktor.ludwig@gmail.com>
2026-06-21 01:18:06 +0200

    fix: more realistic default function

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -6,9 +6,11 @@ export default function App() {
     <View style={styles.container}>
       <BenchMark
         callback={() => {
+          let sum = 0;
           for (let i = 0; i < 1000000; i++) {
-            Math.sqrt(i);
+            sum += Math.sqrt(i);
           }
+          return sum;
         }}
       />
     </View>
